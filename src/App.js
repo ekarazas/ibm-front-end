f26f2a45e3b743c9646a9f21bc12900fecd9cac0
ekarazas <karazas7@gmail.com>
2021-07-09 00:30:09 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
-import Button from "react-bootstrap/Button";
 
 const App = () => {
   return (
     <>
-      <h1>Hello React</h1>
-      <Button variant="primary">This is my button</Button>
+      <header>
+        <h1>GNews</h1>
+      </header>
     </>
   );
 };
