901430fe84729d49ca1913fbc87dc4c4bfba1469
ekarazas <karazas7@gmail.com>
2021-07-08 21:15:08 +0300

    installed bootstrap and sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+import Button from "react-bootstrap/Button";
 
 const App = () => {
-  return <h1>Hello React</h1>;
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Button variant="primary">This is my button</Button>
+    </>
+  );
 };
 
 export default App;
